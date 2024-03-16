0dd230a9beee91b1470a4466e38a771fd13f475a
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-03-16 01:44:40 -0600

    added patch and delete methods

--- employerAPI.js
+++ employerAPI.js
@@ -22,9 +22,37 @@ server.post("/employees", (req, res) => {
   employees.push(newEmployee);
   res.send(newEmployee, 201); // second argument specifies status code: 201 means new resourse created
 });
+server.patch("/api/employees/:id", function (req, res) {
+  const foundEmployee = employees.find(
+    (employee) => employee.id == parseInt(req.params.id)
+  );
+  // if the course is not found, return a 404 error
+  if (foundEmployee == null) {
+    res.status(404).send("employee not found");
+    return;
+  }
+  if (foundEmployee) {
+    for (let i in req.body) {
+      foundEmployee[i] = req.body[i];
+    }
+    return res.status(200).send(foundEmployee);
+  }
+  return res.status(404).send("wrong ID, employee not found.");
+});
 
+server.delete("/employees/delete/:id", (req, res) => {
+  const foundEmployee = employees.find(
+    (employee) => employee.id == parseInt(req.params.id)
+  );
+
+  const foundEmployeeIndex = employees.indexOf(foundEmployee);
+
+  if (!foundEmployee) {
+    return res.status(404).send("employee not found.");
+  }
+  employees.splice(foundEmployeeIndex, 1);
+  return res.status(200).send(foundEmployee);
+});
 server.listen(3000, () => {
   console.log("Server working!");
 });
-
-// do the rest yourself!!!
